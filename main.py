0a3e776b991ccc9a955f767c0f1de40d36e357b5
Bec Chao <becchao@gmail.com>
2025-08-22 17:46:25 +1000

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from calculator.add import add
+from calculator.subtract import subtract
+from utils.format import pretty_print
+
+def run_demo():
+    x, y = 10, 5
+    results = {
+        "add": add(x, y),
+        "subtract": subtract(x, y),
+    }
+    pretty_print(results)
+
+if __name__ == "__main__":
+    run_demo()
---
+++ utils/format.py
@@ -0,0 +1,3 @@
+def pretty_print(results: dict):
+    for operation, result in results.items():
+        print(f"{operation.upper()}: {result}")
